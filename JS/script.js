11fae9c766dc99d4b06b866929d89d97c6fea8f7
Kristopher Owen <kristopherowen11@gmail.com>
2025-03-17 14:04:49 +0700

    Semangat

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,30 @@
-document.getElementsByTagName('body') [0].style.backgroundColor = 'Yellow';
+document.getElementsByTagName('body')[0].style.backgroundColor = 'Yellow';
+
+// Variabel
+let nama = "Jovan Tek";
+let Umur  = 20;
+let statusMahasiswa = true;
+
+document.getElementById('nama').innerHTML = "Nama saya : " + nama;
+document.getElementById('Umur').innerHTML = `Umur : ${Umur}`;
+
+//array
+let mahasiswa = ["Ahmad", "Budi", "Caca", "Jovan"
+];
+mahasiswa.forEach( (mhs) => {
+    document.getElementById(`mahasiswa`).innerHTML += `<li>${mhs} </li>`;
+});
+
+let nilai = [
+    {nama: "Ahmad", nilai: 80},
+    {nama: "Budi", nilai: 90},
+    {nama: "Caca", nilai: 75},
+    {nama: "Jovan", nilai: 20}
+];
+
+nilai.forEach((data) => {
+document.getElementById('nilai').innerHTML += `<tr>
+    <td>${data.nama}    </td>
+    <td>${data.nilai}</td>
+    </tr>`;
+});
